a43bbb879ef32731cf8c40eb75876f1eb8d6f599
Tanzim rahman <tanzim008@gmail.com>
2025-09-06 03:04:11 -0230

    created the react app with npx create react

---
+++ src/components/homepage/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Slider from './Slider'
+import About from './About'
+import Projects from './Projects'
+const Home = () => {
+  return (
+    <>
+        <Slider/>
+        <About/>
+        <Projects/>
+    </>
+  )
+}
+
+export default Home
